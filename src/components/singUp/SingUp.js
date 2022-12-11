8accbbfd24cfe67a45b94d0e8c6e5a088a5dd7b8
gamzenurtukel <gamzenurtukel@gmail.com>
2022-12-11 21:38:29 +0300

    elements added

--- src/components/singUp/SingUp.js
+++ src/components/singUp/SingUp.js
@@ -1,7 +1,37 @@
 import "./SingUp.scss";
 
+import LottieSingUp from "../lottieSingUp/LottieSingUp";
+
 function SingUp() {
-  return <div>sing up</div>;
+  return (
+    <div className="sing-up">
+      <div className="sing-up-container">
+        <div className="sing-up-lottie">
+          <LottieSingUp />
+        </div>
+        <div className="sing-up-form">
+          <form className="form-container">
+            <input
+              className="form-input-username"
+              placeholder="User name"
+              name="username"
+            />
+            <input
+              className="form-input-email"
+              placeholder="E-mail"
+              name="email"
+            />
+            <input
+              className="form-input-password"
+              placeholder="Password"
+              name="password"
+            />
+            <button className="form-button">Sing Up</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default SingUp;
