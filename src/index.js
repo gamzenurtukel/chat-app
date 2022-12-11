9fd21d89fd706c58624d10d9f5a5110fd712e141
gamzenurtukel <gamzenurtukel@gmail.com>
2022-12-12 00:01:06 +0300

    page route created

--- src/index.js
+++ src/index.js
@@ -3,12 +3,21 @@ import ReactDOM from "react-dom/client";
 import "./index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./components/login";
+import SingUp from "./components/singUp";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <React.StrictMode>
+      <Routes>
+        <Route exact path="/login" element={<Login />} />
+        <Route path="/singUp" element={<SingUp />} />
+        <Route path="/*" element={<App />} />
+      </Routes>
+    </React.StrictMode>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
