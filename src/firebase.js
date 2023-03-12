ff1f9193048feebc50b2466385723ff8892e710d
Gamzenur Tükel <92482243+gamzenurtukel@users.noreply.github.com>
2023-03-12 22:12:34 +0300

    firebase configuration added

---
+++ src/firebase.js
@@ -0,0 +1,28 @@
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  updateProfile,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+} from 'firebase/auth';
+import { getDatabase } from 'firebase/database';
+import { getStorage } from 'firebase/storage';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCVEbTTMrk1CiUp08jkI5b1teGcwXC8RMo',
+  authDomain: 'chat-app-d2b69.firebaseapp.com',
+  projectId: 'chat-app-d2b69',
+  storageBucket: 'chat-app-d2b69.appspot.com',
+  messagingSenderId: '113922878716',
+  appId: '1:113922878716:web:ab53356dd2e9d573877b79',
+  measurementId: 'G-RH1TR7J1G1',
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const database = getDatabase(app);
+export const storage = getStorage(app);
