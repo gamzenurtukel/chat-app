b11652bad7ce006ed94f47e9b13ce764ca495b64
Gamzenur Tükel <92482243+gamzenurtukel@users.noreply.github.com>
2023-04-02 15:38:41 +0300

    component changed to onAuthStateChanged status

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import './App.scss';
+
 import LandingPage from './pages/landingPage';
+import 'antd/dist/reset.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { login, logout, selectUser } from '../src/redux/slices/userSlice';
+import { useEffect } from 'react';
+import { auth, onAuthStateChanged } from './firebase';
+import Login from './pages/Authentication/login';
 
 function App() {
-  return (
-    <div>
-      <LandingPage />
-    </div>
-  );
+  const dispatch = useDispatch();
+  const user = useSelector(selectUser);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (userAuth) => {
+      if (userAuth) {
+        dispatch(login({ uid: userAuth.uid, email: userAuth.email }));
+      } else {
+        dispatch(logout());
+      }
+    });
+  }, []);
+
+  return <div>{!user ? <Login /> : <LandingPage />}</div>;
 }
 
 export default App;
